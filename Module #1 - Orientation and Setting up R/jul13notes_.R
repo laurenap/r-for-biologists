5087f8000e01f142b3eda6fbf2c1238da5524e74
Lauren Pearson <l.pearson@mail.utoronto.ca>
2025-07-13 12:29:24 -0400

    Update jul13notes_.R

--- Module #1 - Orientation and Setting up R/jul13notes_.R
+++ Module #1 - Orientation and Setting up R/jul13notes_.R
@@ -1,1 +1,16 @@
+# make comments inside codenotebook with hashmark (functions as an uncomment)
+# Careful documentation is important for creating reproducible analysis 
 
+# for example 
+
+[Link Text](https://colauttilab.github.io/RCrashCourse/fundamentals.html"RCrashCourse")
+
+# Use hastags to make comments - not read by the R console
+# Use other characters and blank lines to improve readability:
+# ------------------------- 
+# My first R script 
+# Today's Date
+# -------------------------
+# Add a summary description of what the script does
+# This script will...
+# And annotate individual parts of the script
